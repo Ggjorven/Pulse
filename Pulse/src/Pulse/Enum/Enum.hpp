b892e3971ed4d64068a63036240a3104579b957d
ggjorven <jorbenvanderwal@gmail.com>
2024-08-14 22:53:50 +0200

    Add extra enum utils.

--- Pulse/src/Pulse/Enum/Enum.hpp
+++ Pulse/src/Pulse/Enum/Enum.hpp
@@ -166,4 +166,71 @@ namespace Pulse::Enum
 
 	}
 
+    // From https://github.com/Reputeless/EnumBitmask.git CC0-1.0 Universal LICENSE
+    namespace EnumBitmask
+    {
+        template<typename Enum>
+        struct EnumWrapper
+        {
+        public:
+            constexpr explicit operator bool() const noexcept
+            {
+                return (e != Enum{ 0 });
+            }
+
+            constexpr operator Enum() const noexcept
+            {
+                return e;
+            }
+
+        public:
+            Enum e;
+        };
+
+        template<typename Enum>
+        EnumWrapper(Enum e)->EnumWrapper<Enum>;
+    }
+
+    #define PULSE_BIT_OPERATORS(ENUM)								    \
+	static_assert(std::is_enum_v<ENUM>, "ENUM must be an enum type");	\
+	[[nodiscard]]														\
+	inline constexpr auto operator &(ENUM lhs, ENUM rhs) noexcept		\
+	{																	\
+		using U = std::underlying_type_t<ENUM>;							\
+		return EnumBitmask::EnumWrapper{								\
+			ENUM(static_cast<U>(lhs) & static_cast<U>(rhs))};			\
+	}																	\
+	[[nodiscard]]														\
+	inline constexpr auto operator |(ENUM lhs, ENUM rhs) noexcept		\
+	{																	\
+		using U = std::underlying_type_t<ENUM>;							\
+		return EnumBitmask::EnumWrapper{								\
+			ENUM(static_cast<U>(lhs) | static_cast<U>(rhs))};			\
+	}																	\
+	[[nodiscard]]														\
+	inline constexpr auto operator ^(ENUM lhs, ENUM rhs) noexcept		\
+	{																	\
+		using U = std::underlying_type_t<ENUM>;							\
+		return EnumBitmask::EnumWrapper{								\
+			ENUM(static_cast<U>(lhs) ^ static_cast<U>(rhs)) };			\
+	}																	\
+	[[nodiscard]]														\
+	inline constexpr ENUM operator ~(ENUM value) noexcept				\
+	{																	\
+		using U = std::underlying_type_t<ENUM>;							\
+		return ENUM(~static_cast<U>(value));							\
+	}																	\
+	inline constexpr ENUM& operator &=(ENUM& lhs, ENUM rhs) noexcept	\
+	{																	\
+		return lhs = (lhs & rhs);										\
+	}																	\
+	inline constexpr ENUM& operator |=(ENUM& lhs, ENUM rhs) noexcept	\
+	{																	\
+		return lhs = (lhs | rhs);										\
+	}																	\
+	inline constexpr ENUM& operator ^=(ENUM& lhs, ENUM rhs) noexcept	\
+	{																	\
+		return lhs = (lhs ^ rhs);										\
+	}
+
 }
