7145842280c0860cc8f89491a4b63ec147889ae7
ggjorven <Jorbenvanderwal@gmail.com>
2024-09-20 20:13:42 +0200

    Add back InheritsFrom.

--- Pulse/src/Pulse/Types/TypeUtils.hpp
+++ Pulse/src/Pulse/Types/TypeUtils.hpp
@@ -9,10 +9,20 @@ namespace Pulse::Types
 {
 
     //////////////////////////////////////////////////
-    // Other
+    // Utils
     //////////////////////////////////////////////////
     template<typename T>
     using Clean = std::decay_t<T>;
+
+    //////////////////////////////////////////////////
+    // Main
+    //////////////////////////////////////////////////
+    template <typename Base, typename Derived>
+    struct InheritsFromImpl : std::is_base_of<Base, Derived> {};
+
+    // Checks if Derived inherits from Base
+    template <typename Base, typename Derived>
+    inline constexpr bool InheritsFrom = InheritsFromImpl<Base, Derived>::value;
 
     template <typename Base, typename Derived>
     struct SameImpl : std::is_same<Base, Derived> {};
@@ -21,8 +31,9 @@ namespace Pulse::Types
     template <typename T1, typename T2>
     inline constexpr bool Same = SameImpl<T1, T2>::value;
 
-
-
+    //////////////////////////////////////////////////
+    // Other
+    //////////////////////////////////////////////////
     template<typename... Types>
     struct TypeGroup {};
 
